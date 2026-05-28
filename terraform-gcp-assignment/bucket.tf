fdd94bf4703cb7c57f8e3f249b42640cfba80349
adewunmivictor5 <adewunmivictor5@gmail.com>
2026-05-28 15:31:19 +0100

    Add Terraform GCP resources: subnets, VMs, buckets and DB instances

---
+++ terraform-gcp-assignment/bucket.tf
@@ -0,0 +1,18 @@
+hcl
+# Bucket 1 - US
+resource "google_storage_bucket" "bucket_us" {
+  name          = "devops-bucket-us-debodinho"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+# Bucket 2 - EU
+resource "google_storage_bucket" "bucket_eu" {
+  name          = "devops-bucket-eu-debodinho"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
---
+++ terraform-gcp-assignment/vpc.tf
@@ -0,0 +1,32 @@
+hcl
+# Existing VPC
+resource "google_compute_network" "my_vpc" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false
+}
+
+# Subnet 1 - us-east1
+resource "google_compute_subnetwork" "subnet_3" {
+  name          = "subnet-3"
+  ip_cidr_range = "10.0.3.0/24"
+  region        = "us-east1"
+  network       = google_compute_network.my_vpc.id
+}
+
+# Subnet 2 - europe-west1
+resource "google_compute_subnetwork" "subnet_4" {
+  name          = "subnet-4"
+  ip_cidr_range = "10.0.4.0/24"
+  region        = "europe-west1"
+  network       = google_compute_network.my_vpc.id
+}
+
+# Firewall rule for HTTP/HTTPS
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
+  network = google_compute_network.my_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
